94b0432e24f45ce2c04e52cda332e8c9bfca0c8e
Sheffield Turner <sturner93@hotmail.com>
2018-08-20 10:26:24 -0500

    Add project files.

---
+++ EnrollmentApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EnrollmentApp.Startup))]
+namespace EnrollmentApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
