d95d9dcc4511f5a245b7f855c3d9611d8683400e
Sheffield Turner <sturner93@hotmail.com>
2018-09-11 21:57:52 -0500

    Added required validation for apply form

--- EnrollmentApp/Models/Student.cs
+++ EnrollmentApp/Models/Student.cs
@@ -9,24 +9,37 @@ namespace EnrollmentApp.Models
     public class Student
     {
             public int StudentID { get; set; }
+            [Required]
             public string UserName { get; set; }
             public int AddressID { get; set; }
             public int MajorID { get; set; }
+            [Required]
             public string FirstName { get; set; }
+            [Required]
             public string LastName { get; set; }
+            [Required]
             public string Gender { get; set; }
+            [Required]
             public string Major { get; set; }
             public string Year { get; set; }
+            [Required]
             public string DOB { get; set; }
+            [Required]
             [DataType(DataType.PhoneNumber)]
             [RegularExpression("^(\\([0-9]{3}\\) |[0-9]{3}-)[0-9]{3}-[0-9]{4}$", ErrorMessage = "Please enter valid phone no.")]
             public string PhoneNumber { get; set; }
+            [Required]
             public string Email { get; set; }
             public decimal GPA { get; set; }
+            [Required]
             public string Street { get; set; }
+            [Required]
             public string City { get; set; }
+            [Required]
             public string Zip { get; set; }
+            [Required]
             public string State { get; set; }
+            [Required]
             public string Country { get; set; }
 
             public string FullName
